6606624ca0dab5f9dbee5926e36c3dd15b6b954b
Daan Hilly <daan.hillebrand01@gmail.com>
2026-03-14 12:10:44 +0100

    feat: CORS for localHost backend

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,5 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactDevServer", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddControllers();
 builder.Services.AddMemoryCache();
 builder.Services.AddHttpClient<ITriviaApiService, TriviaApiService>();
@@ -7,7 +17,10 @@ builder.Services.AddScoped<ITriviaService, TriviaService>();
 builder.Services.AddSingleton<IStorageProvider, InMemoryStorageProvider>();
 builder.Services.AddScoped<ITriviaStorageService, TriviaStorageService>();
 
+
 var app = builder.Build();
+
+app.UseCors("AllowReactDevServer");
 
 app.MapControllers();
 app.Run();
